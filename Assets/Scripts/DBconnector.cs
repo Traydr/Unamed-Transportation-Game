3c15cb79aa425e8a1707ceb1dd3a34730e53353b
Traydr <Titas.Lukaitis@gmail.com>
2021-02-11 10:06:06 +0100

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/DBconnector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data.SQLite;
+
+public class DBconnector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SQLiteConnection connection =
+                 new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
+        connection.Open();
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
+                          "  'id' INTEGER PRIMARY KEY, " +
+                          "  'name' TEXT NOT NULL, " +
+                          "  'score' INTEGER NOT NULL" +
+                          ");";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
---
+++ Assets/Scripts/MouseClickDetection.cs
@@ -0,0 +1,45 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseClickDetection : MonoBehaviour
+{
+    public Transform player; public Transform selfObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MouseClickDetection.Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log("Registered_Click_1");
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                string tempTag = hit.transform.tag;
+                Debug.Log("Registered_Click_2");
+                switch (tempTag)
+                {
+                    case "Shops":
+                        break;
+                    case "Cities":
+                        break;
+                    default:
+                        Debug.Log("Did not hit A city or shop");
+                        break;
+
+                }
+            }
+            else { Debug.Log("No_Click"); }
+        }
+    }
+}
