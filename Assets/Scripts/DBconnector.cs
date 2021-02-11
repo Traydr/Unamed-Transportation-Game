c16a369a7ab903f4ebefe26e5164035313dff4d9
Traydr <Titas.Lukaitis@gmail.com>
2021-02-11 11:21:52 +0100

    Added functionality to DBconnector.cs script
    In process of adding SQL commands to be executed in the script.

--- Assets/Scripts/DBconnector.cs
+++ Assets/Scripts/DBconnector.cs
@@ -8,17 +8,27 @@ public class DBconnector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SQLiteConnection connection =
-                 new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
+        SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
         connection.Open();
-        SQLiteCommand command = connection.CreateCommand();
-        command.CommandType = System.Data.CommandType.Text;
-        command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
+        SQLiteCommand cmd = connection.CreateCommand();
+
+
+        cmd.CommandType = System.Data.CommandType.Text;
+        cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
                           "  'id' INTEGER PRIMARY KEY, " +
                           "  'name' TEXT NOT NULL, " +
                           "  'score' INTEGER NOT NULL" +
                           ");";
-        command.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO highscores (id,name,score) VALUES (0,User,0)";
+
+        cmd.ExecuteNonQuery();
         connection.Close();
+    }
+
+    void DBInsert(string table, string tableAttributes, string dataIn)
+    {
+        string combinedCommand = "INSERT INTO ";
+        combinedCommand += table + " (" + tableAttributes + ") VALUES (" + dataIn + ;
     }
 }
