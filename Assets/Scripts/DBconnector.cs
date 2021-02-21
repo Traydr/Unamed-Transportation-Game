ac4cd224e887311cbed0dc95a46ae2c27eb45bdd
Traydr <Titas.Lukaitis@gmail.com>
2021-02-22 00:09:23 +0100

    ART + DBconnector.cs
    Addition of new SHOP and CITY art.
    DBconnector.cs now has DBDelete and DBSelect, an update has been done to DBInsert as well

--- Assets/Scripts/DBconnector.cs
+++ Assets/Scripts/DBconnector.cs
@@ -12,11 +12,13 @@ using System.Data.SQLite;
  * 
  * 
  * 
- * 
+ *  - REMEMBER TO CHANGE THE DIRECTORIES OF THE // SQLiteConnection // TO FINAL DATABASE LOCATION AND FILE NAME
  */
 
 public class DBconnector : MonoBehaviour
 {
+    
+
     void Start()
     {
         // Tests for database commands, all commands should eventually get their own functions!
@@ -24,7 +26,7 @@ public class DBconnector : MonoBehaviour
         connection.Open();
         SQLiteCommand cmd = connection.CreateCommand();
 
-
+        /* Written Originally For Testing
         cmd.CommandType = System.Data.CommandType.Text;
         cmd.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
                           "  'id' INTEGER PRIMARY KEY, " +
@@ -33,19 +35,45 @@ public class DBconnector : MonoBehaviour
                           ");";
 
         cmd.CommandText = "INSERT INTO highscores (id,name,score) VALUES (0,User,0)";
+        */
+        cmd.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    void DBInsert(string table, string columnName, string dataIn)
+    {
+        SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
+        connection.Open();
+        SQLiteCommand cmd = connection.CreateCommand();
+
+        string combinedCommand = string.Format("INSERT INTO {0} ({1}) VALUES ({2})", table, columnName, dataIn);
+        cmd.CommandText = combinedCommand;
 
         cmd.ExecuteNonQuery();
         connection.Close();
     }
 
-    void DBInsert(string table, string tableAttributes, string dataIn)
+    void DBDelete(string table, string columnName, string dataToDelete)
     {
         SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
         connection.Open();
-
         SQLiteCommand cmd = connection.CreateCommand();
-        string combinedCommand = "INSERT INTO ";
-        combinedCommand += table + " (" + tableAttributes + ") VALUES (" + dataIn + ")";
+
+        string combinedCommand = string.Format("DELETE FROM {0} WHERE {1} = {2}", table, columnName, dataToDelete);
+        cmd.CommandText = combinedCommand;
+
+        cmd.ExecuteNonQuery();
+        connection.Close();
+
+    }
+
+    void DBSelect(string table, string columnName)
+    {
+        SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\titas\Desktop\Games & Programs\ProjectFiles\Databases\testdb.db;Version=3;");
+        connection.Open();
+        SQLiteCommand cmd = connection.CreateCommand();
+
+        string combinedCommand = string.Format("SELECT {0} FROM {1}", columnName, table);
         cmd.CommandText = combinedCommand;
 
         cmd.ExecuteNonQuery();
