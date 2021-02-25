9961c91b01b78812414b0aa0976d7bb9e5b4e1a8
Traydr <Titas.Lukaitis@gmail.com>
2021-02-25 11:18:41 +0100

    Final Commit + RoadGen + Small changes
    Final Unity commit for today
    Start of the Road Generation script
    Small change to the escape menu

---
+++ Assets/Scripts/RoadGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This script is intended to only ever run once when the game is running / when a save file is loaded
+ * Generate black rectangles between 2 points.
+ * 
+ */
+public class RoadGen : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+}
