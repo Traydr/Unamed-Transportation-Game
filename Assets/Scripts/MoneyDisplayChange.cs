0a718c02940719ccf82d5cf9db83e09d81664b09
Traydr <Titas.Lukaitis@gmail.com>
2021-03-05 15:09:12 +0100

    MoneyDisplay + Font + small changes
    Money amount now displayed on the top right.
    New script MoneyDisplayChange attacthed to GameHandler
    New Fonts folder with 'Roboto' font
    Scene name updated to MaingGame

---
+++ Assets/Scripts/MoneyDisplayChange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplayChange : MonoBehaviour
+{
+    public Text money;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MoneyDisplayChange Initited");
+        money.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            int currentMoney = int.Parse(money.text);
+            currentMoney += 100;
+            money.text = currentMoney.ToString();
+        }
+        else { }
+    }
+}
