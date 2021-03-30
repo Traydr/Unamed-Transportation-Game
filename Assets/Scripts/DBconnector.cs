2d9b6601d1f7709b6a75da668db389171aeff4e8
Traydr <Titas.Lukaitis@gmail.com>
2021-03-30 21:34:54 +0200

    Location Table + DBconnector script
    Updates to location table for correct bool values
    
    DBconnector script updated so that each table will have its own set of commands

--- Assets/Scripts/DBconnector.cs
+++ Assets/Scripts/DBconnector.cs
@@ -5,13 +5,23 @@ using System.Data.SQLite;
 
 public class DBconnector : MonoBehaviour
 {
-    
+    /* NOTES FOR SCRIPT
+     * - EACH TABLE NEEDS COMMAND THAT ARE ONLY NEEDED FOR ITSELF
+     * 
+     */
 
     void Start()
     {
         Debug.Log("DBconnector.Start");
+
+        // Testing
+        string[,] arrayOfTable = new string[20, 20];
+        arrayOfTable = DBSelect("Location","LocationID");
+
+        // End of Testing
     }
 
+    // SECTION: COPY PASTE [START OF SECTION - TO BE DELETED]
     void DBInsert(string table, string columnName, string dataIn)
     {
         SQLiteConnection connection = new SQLiteConnection(@"Data Source=Assets/DataBase/UnamedTransportationGame.db;Version=3;");
@@ -44,22 +54,22 @@ public class DBconnector : MonoBehaviour
         SQLiteConnection connection = new SQLiteConnection(@"Data Source=Assets/DataBase/UnamedTransportationGame.db;Version=3;");
         connection.Open();
         SQLiteCommand cmd = connection.CreateCommand();
-        
+
         //SELECT STATEMENT
-        string combinedCommand = string.Format("SELECT {0} FROM {1}", columnName, table);
+        //string combinedCommand = string.Format("SELECT {0} FROM {1}", columnName, table);
+        string combinedCommand = string.Format("SELECT * FROM {0} ORDER BY {1} ASC", table, columnName);
         cmd.CommandText = combinedCommand;
 
         // SQL DATA READER
         SQLiteDataReader sqlReader = cmd.ExecuteReader();
         string[,] arrayOfTable = new string[sqlReader.FieldCount,10];
-        int indexCounter = 0;
 
         while (sqlReader.Read())
         {
-            Debug.Log(sqlReader.GetString(0));
+            string logDebug = string.Format("{0}, {1}, {2}, {3}", sqlReader.GetInt32(0), sqlReader.GetString(1), sqlReader.GetString(2), sqlReader.GetString(3));
+            Debug.Log(logDebug);
         }
 
-        cmd.ExecuteNonQuery();
         connection.Close();
 
         return arrayOfTable;
@@ -76,5 +86,74 @@ public class DBconnector : MonoBehaviour
 
         cmd.ExecuteNonQuery();
         connection.Close();
+    }
+    // END OF SECTION: COPY PASTE
+
+
+    // ALL TABLES
+    // LOCATION (Shorthand: LT)
+
+    public string[,] DBLTSelect()
+    {
+        string[,] selectionResult = new string[6, 4];
+
+
+
+        return selectionResult;
+    }
+
+    // PRODUCTS (Shorthand: PD)
+
+    public string[,] DBPDSelect()
+    {
+        string[,] selectionResult = new string[7, 6];
+
+
+
+        return selectionResult;
+    }
+
+    // PRODUCTLOCATION (Shorthand: PDLT)
+
+    public string[,] DBPDLTSelect()
+    {
+        string[,] selectionResult = new string[24, 5];
+
+
+
+        return selectionResult;
+    }
+
+    // PRODUCTCHANGES (Shorthand: PDCH)
+
+    public string[,] DBPDCHSelect()
+    {
+        string[,] selectionResult = new string[50, 6];
+
+
+
+        return selectionResult;
+    }
+
+    // PLAYER (Shorthand: PL)
+
+    public string[,] DBPLSelect()
+    {
+        string[,] selectionResult = new string[10, 4];
+
+
+
+        return selectionResult;
+    }
+
+    // UPGRADES (Shorthand: UG)
+
+    public string[,] DBUPSelect()
+    {
+        string[,] selectionResult = new string[10, 7];
+
+
+
+        return selectionResult;
     }
 }
