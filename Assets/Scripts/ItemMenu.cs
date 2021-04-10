969bf718df904fac495f0f2f8601cae5b6f023f9
Traydr <Titas.Lukaitis@gmail.com>
2021-04-10 18:58:19 +0200

    Unity Close
    + Removal of unused source

--- Assets/Scripts/ItemMenu.cs
+++ Assets/Scripts/ItemMenu.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ItemMenu : MonoBehaviour
 {
