36bfb7ccfe22b8b0b4825abc0810d7e540cabf6c
Traydr <Titas.Lukaitis@gmail.com>
2021-04-10 17:49:58 +0200

    First Buildable version
    Encountered problems with build
    
    canvas seemed to be shrunk down
    
    in game UI was over top sprites and escape menu seemed shrunk
    
    could not see the sell or buy menu as only the background activated

--- Assets/Scripts/RoadGen.cs
+++ Assets/Scripts/RoadGen.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Net.Sockets;
-using UnityEditor.U2D.Animation;
-using UnityEngine;
+﻿using UnityEngine;
 
 /* This script is intended to only ever run once when the game is running / when a save file is loaded
  * Activate or deactivate pre-made roads. Changing the 2d array of raods that currently exists inside Playermovement.cs
